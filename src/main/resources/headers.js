7cb6448a79ccd13624fe907f1d71d1b9c0d9b4f3
bharathamanik <k.bharathamani@elsevier.com>
2021-03-13 00:25:18 +0000

    add resources

---
+++ src/main/resources/headers.js
@@ -0,0 +1,10 @@
+ function() {
+   var contentType = karate.get('Content_Type')?karate.get('Content_Type'):"application/json"
+   var accept = karate.get('accept')?karate.get('accept'):"*/*"
+   var Authorization = karate.get('Authorization')?karate.get('Authorization'):'Bearer 48a7fe60034be1eb264102c122d31ea48f4ef04e'
+   var headers = {
+                 "Content-Type": contentType,
+                 "Authorization": Authorization,
+                 "Accept": accept,
+                 }
+   return headers}
